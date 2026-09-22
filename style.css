d9bc78941f60239540684f4f9d4f55407b577cdd
Yashwanth Kumar Palle <yaswanth85640@gmail.com>
2026-09-22 15:01:17 +0530

    Add initial styles for body and container

---
+++ style.css
@@ -0,0 +1,18 @@
+body {
+    font-family: Arial;
+    background-color: #f2f2f2;
+    text-align: center;
+}
+
+.container {
+    margin-top: 100px;
+}
+
+h1 {
+    color: #0073e6;
+}
+
+button {
+    padding: 12px 25px;
+    font-size: 18px;
+}
